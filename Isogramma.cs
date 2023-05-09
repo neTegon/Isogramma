419b0c20185aa3be2dfaaa725a3542de6e363ea7
Elettra Tegon <112578860+neTegon@users.noreply.github.com>
2023-05-09 17:19:38 +0200

    Add files via upload

---
+++ Isogramma.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+public static class Isogramma
+{
+    public static bool Verifica(string word)
+    {
+        //non mi funzionano nei test con le doppie
+        string [] parole = word.Split(' ','-');
+        string app= String.Join("",parole).ToLower();
+        for (int i = 0; i < app.Length; i++)
+        {
+            for (int j = i + 1; j < app.Length; j++)
+            {
+                if (app[i] ==  app[j])
+                {
+                    return false;
+                }  
+            }
+        }
+        return true;
+    }
+
+}
